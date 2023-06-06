e74a0c17581c370bd8d46c3f96c458adb7b929cc
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2023-06-06 22:55:22 +0900

    타이틀 추가, 리소스 추가

--- Game/app/src/main/java/com/example/lifegame/app/TitleActivity.java
+++ Game/app/src/main/java/com/example/lifegame/app/TitleActivity.java
@@ -1,15 +1,30 @@
 package com.example.lifegame.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.lifegame.R;
 
 
 public class TitleActivity extends AppCompatActivity {
 
+
+    private static final String TAG = TitleActivity.class.getSimpleName();
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
     }
+
+    public void onBtnStartGame(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
