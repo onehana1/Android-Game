ba2df930d0e12b00dbb92a2c6c106d373ca45d43
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2023-06-06 20:30:12 +0900

    타이틀 activity 생성

---
+++ Game/app/src/main/java/com/example/lifegame/app/TitleActivity.java
@@ -0,0 +1,15 @@
+package com.example.lifegame.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lifegame.R;
+
+
+public class TitleActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+}
